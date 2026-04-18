156956b7bcda1ee32b00226ccbcbb70930e99f1c
CogNoman <cognoman@hotmail.com>
2026-04-18 17:22:52 -0400

    Adding fog

--- Assets/Scripts/Environment/SnowFollowPlayer.cs
+++ Assets/Scripts/Environment/SnowFollowPlayer.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class SnowFollowPlayer : MonoBehaviour
 {
     public Transform player;
-    public float followSpeed = 2f;
+    public float followSpeed = 0f;  //This is how much the block of particles lags behind
+                                    //player's movement (higher number = more lag)
     
     void LateUpdate()
     {
