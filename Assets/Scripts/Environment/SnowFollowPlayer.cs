b8f61b7925e92f449a81ca568230b1daae6f6042
CogNoman <cognoman@hotmail.com>
2026-04-18 14:45:54 -0400

    Outer snow particles follow player correctly

--- Assets/Scripts/Environment/SnowFollowPlayer.cs
+++ Assets/Scripts/Environment/SnowFollowPlayer.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class SnowFollowPlayer : MonoBehaviour
 {
     public Transform player;
+    public float followSpeed = 2f;
     
     void LateUpdate()
     {
-        transform.position = player.position;
+        Vector3 target = player.position;
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            target,
+            Time.deltaTime * followSpeed
+        );
     }
 }
