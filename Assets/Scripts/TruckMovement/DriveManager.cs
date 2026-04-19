29e9464e9c158b898ac231569d59c0153612407a
CogNoman <cognoman@hotmail.com>
2026-04-19 00:52:49 -0400

    Added method to stop driving

--- Assets/Scripts/TruckMovement/DriveManager.cs
+++ Assets/Scripts/TruckMovement/DriveManager.cs
@@ -2,13 +2,30 @@ using UnityEngine;
 
 public class DriveManager : MonoBehaviour
 {
+    
+    // To stop the driving, call DriveManager.Instance.StopDriving();
     public static DriveManager Instance;
 
     public bool isDriving = true;
+
     public float speed = 10f;
+    private float defaultSpeed;
 
     void Awake()
     {
         Instance = this;
+        defaultSpeed = speed;
+    }
+
+    public void StopDriving()
+    {
+        isDriving = false;
+        speed = 0f;
+    }
+
+    public void StartDriving()
+    {
+        isDriving = true;
+        speed = defaultSpeed;
     }
 }
